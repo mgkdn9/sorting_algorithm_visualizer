1e38f968e02be28fa1041f3f372ea11a4b4a8d51
Mike Kohlberg <mkohlberg95@gmail.com>
2025-07-31 06:58:45 -0600

    BrowserRouter basename="/sorting-algorithm-visualizer-frontend"

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/sorting-algorithm-visualizer-frontend">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
