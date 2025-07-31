b8445229f116e021fcd992c5fe561887d61c75a4
Mike Kohlberg <mkohlberg95@gmail.com>
2025-07-31 07:04:49 -0600

    404

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter basename="/sorting-algorithm-visualizer-frontend">
