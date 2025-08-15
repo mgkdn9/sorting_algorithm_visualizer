04256d8d695966865a2e9262519994525f44c426
Mike Kohlberg <mkohlberg95@gmail.com>
2025-08-15 09:20:35 -0600

    test. hard to see without using phone

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -7,61 +7,50 @@ const linkStyle = {
   color: "white",
   textDecoration: "none",
 };
+
 const authenticatedOptions = (
   <>
-    <Nav.Link>
-      <Link to="my-scores" style={linkStyle}>
-        My Scores
-      </Link>
+    <Nav.Link as={Link} to="my-scores" style={linkStyle}>
+      My Scores
     </Nav.Link>
-    <Nav.Link>
-      <Link to="change-password" style={linkStyle}>
-        Change Password
-      </Link>
+    <Nav.Link as={Link} to="change-password" style={linkStyle}>
+      Change Password
     </Nav.Link>
-    <Nav.Link>
-      <Link to="sign-out" style={linkStyle}>
-        Log out
-      </Link>
+    <Nav.Link as={Link} to="sign-out" style={linkStyle}>
+      Log out
     </Nav.Link>
   </>
 );
 
 const unauthenticatedOptions = (
   <>
-    <Nav.Link>
-      <Link to="sign-up" style={linkStyle}>
-        Sign Up
-      </Link>
+    <Nav.Link as={Link} to="sign-up" style={linkStyle}>
+      Sign Up
     </Nav.Link>
-    <Nav.Link>
-      <Link to="sign-in" style={linkStyle}>
-        Log In
-      </Link>
+    <Nav.Link as={Link} to="sign-in" style={linkStyle}>
+      Log In
     </Nav.Link>
   </>
 );
 
 const Header = ({ user }) => (
-  <Navbar bg="secondary" fixed="top" variant="dark" expand="sm">
-    <Navbar.Brand>
-      <Nav.Link>
-        <Link to="/" style={linkStyle}>
-          AlgoViews
-        </Link>
-      </Nav.Link>
+  <Navbar
+    bg="secondary"
+    fixed="top"
+    variant="dark"
+    expand="sm"
+    collapseOnSelect
+  >
+    <Navbar.Brand as={Link} to="/" style={linkStyle}>
+      AlgoViews
     </Navbar.Brand>
-    <Nav.Item>
-      <Nav.Link>
-        <Link to="/algo-test" style={linkStyle}>
-          Test Your Knowledge
-        </Link>
-      </Nav.Link>
-    </Nav.Item>
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-      <Nav className="ml-auto justify-content-center">
+      <Nav className="ml-auto">
+        <Nav.Link as={Link} to="/algo-test" style={linkStyle}>
+          Test Your Knowledge
+        </Nav.Link>
         {user && (
           <span className="navbar-text mr-2">Welcome, {user.email}</span>
         )}
