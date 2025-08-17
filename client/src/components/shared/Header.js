9771155f67c11e26d7c03b0e2f9eecbd3abcb09f
Mike Kohlberg <mkohlberg95@gmail.com>
2025-08-17 16:45:23 -0600

    header collapses  on small screens as expected

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -1,63 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
-const linkStyle = {
-  color: "white",
-  textDecoration: "none",
+const Header = ({ user }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => setExpanded(!expanded);
+  const closeNavbar = () => setExpanded(false); // collapse navbar after click
+
+  const linkStyle = { color: "white" };
+
+  const authLinks = (
+    <>
+      <Nav.Link as={Link} to="/my-scores" style={linkStyle} onClick={closeNavbar}>
+        My Scores
+      </Nav.Link>
+      <Nav.Link as={Link} to="/change-password" style={linkStyle} onClick={closeNavbar}>
+        Change Password
+      </Nav.Link>
+      <Nav.Link as={Link} to="/sign-out" style={linkStyle} onClick={closeNavbar}>
+        Log out
+      </Nav.Link>
+    </>
+  );
+
+  const guestLinks = (
+    <>
+      <Nav.Link as={Link} to="/sign-up" style={linkStyle} onClick={closeNavbar}>
+        Sign Up
+      </Nav.Link>
+      <Nav.Link as={Link} to="/sign-in" style={linkStyle} onClick={closeNavbar}>
+        Log In
+      </Nav.Link>
+    </>
+  );
+
+  return (
+    <Navbar
+      bg="secondary"
+      fixed="top"
+      variant="dark"
+      expand="sm"
+      expanded={expanded}
+    >
+      <Navbar.Brand as={Link} to="/" style={{ ...linkStyle, paddingLeft: "10px" }} onClick={closeNavbar}>
+        AlgoViews
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleExpanded} />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/algo-test" style={linkStyle} onClick={closeNavbar}>
+            Test Your Knowledge
+          </Nav.Link>
+          {user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+          {user ? authLinks : guestLinks}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
-
-const authenticatedOptions = (
-  <>
-    <Nav.Link as={Link} to="my-scores" style={linkStyle}>
-      My Scores
-    </Nav.Link>
-    <Nav.Link as={Link} to="change-password" style={linkStyle}>
-      Change Password
-    </Nav.Link>
-    <Nav.Link as={Link} to="sign-out" style={linkStyle}>
-      Log out
-    </Nav.Link>
-  </>
-);
-
-const unauthenticatedOptions = (
-  <>
-    <Nav.Link as={Link} to="sign-up" style={linkStyle}>
-      Sign Up
-    </Nav.Link>
-    <Nav.Link as={Link} to="sign-in" style={linkStyle}>
-      Log In
-    </Nav.Link>
-  </>
-);
-
-const Header = ({ user }) => (
-  <Navbar
-    bg="secondary"
-    fixed="top"
-    variant="dark"
-    expand="sm"
-    collapseOnSelect
-  >
-    <Navbar.Brand as={Link} to="/" style={{ ...linkStyle, paddingLeft: "10px" }}>
-      AlgoViews
-    </Navbar.Brand>
-
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-      <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/algo-test" style={linkStyle}>
-          Test Your Knowledge
-        </Nav.Link>
-        {user && (
-          <span className="navbar-text mr-2">Welcome, {user.email}</span>
-        )}
-        {user ? authenticatedOptions : unauthenticatedOptions}
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
 
 export default Header;
